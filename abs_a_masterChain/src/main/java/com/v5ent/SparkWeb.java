c3341b76d95311d82af910550f2a068f7e09ccf0
Matthew Z <drmatthew1982@163.com>
2019-04-11 02:13:04 +0800

    abs a master chain

---
+++ abs_a_masterChain/src/main/java/com/v5ent/SparkWeb.java
@@ -0,0 +1,13 @@
+package com.v5ent;
+
+
+import static spark.Spark.port;
+
+import net.matthew.Static_Value;
+
+public class SparkWeb {
+	public static void main(String[] args) {
+		port(Static_Value.ABS_A_MASTER_CHAIN_PORT);
+		CommonMethod.initial();
+	}
+}
