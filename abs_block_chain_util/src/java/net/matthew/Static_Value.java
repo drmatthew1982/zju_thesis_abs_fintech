12fb3585f8447dc115b6d774a26b97180c7d2505
Matthew Z <drmatthew1982@163.com>
2019-04-11 02:26:11 +0800

    block chain util

---
+++ abs_block_chain_util/src/java/net/matthew/Static_Value.java
@@ -0,0 +1,12 @@
+package net.matthew;
+
+public class Static_Value {
+	public static String ABS_A_MASTER_CHAIN_DOMAIN="localhost";
+	public static int ABS_A_MASTER_CHAIN_PORT=4567;
+	public static String ABS_A_CAPITAL_CHAIN_DOMAIN="localhost";
+	public static int ABS_A_CAPITAL_CHAIN_PORT=4568;
+	public static String ABS_A_REPUTATION_CHAIN_DOMAIN="localhost";
+	public static int ABS_A_REPUTATIONL_CHAIN_PORT=4569;
+	public static String ABS_A_VALUE_CHAIN_DOMAIN="localhost";
+	public static int ABS_A_VALUE_CHAIN_PORT=4570;
+}
