626db95167996ff42285b00ff7d66f44bf584ab5
Matthew Z <drmatthew1982@163.com>
2019-04-11 01:55:00 +0800

    common_chain

---
+++ common_chain/src/main/java/com/v5ent/entity/Message.java
@@ -0,0 +1,13 @@
+package com.v5ent.entity;
+
+public class Message {
+	private String vac;
+
+	public String getVac() {
+		return vac;
+	}
+
+	public void setVac(String vac) {
+		this.vac = vac;
+	}
+}
