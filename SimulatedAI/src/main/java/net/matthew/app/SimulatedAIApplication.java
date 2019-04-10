a0d05a487ca60e2f6f68edca825555f16ddd7a58
Matthew Z <drmatthew1982@163.com>
2019-04-11 02:49:02 +0800

    Simulated AI framework

---
+++ SimulatedAI/src/main/java/net/matthew/app/SimulatedAIApplication.java
@@ -0,0 +1,16 @@
+package net.matthew.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class SimulatedAIApplication {
+      public static void main(String[] args) {
+          SpringApplication.run(SimulatedAIApplication.class, args);
+      }
+      
+}
