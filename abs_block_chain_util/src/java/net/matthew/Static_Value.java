82b2901dbf9e2692e0e73916294e175465199cec
Matthew Z <drmatthew1982@163.com>
2019-04-11 16:01:22 +0800

    Static Value modified, method added

--- abs_block_chain_util/src/java/net/matthew/Static_Value.java
+++ abs_block_chain_util/src/java/net/matthew/Static_Value.java
@@ -9,4 +9,10 @@ public class Static_Value {
 	public static int ABS_A_REPUTATIONL_CHAIN_PORT=4569;
 	public static String ABS_A_VALUE_CHAIN_DOMAIN="localhost";
 	public static int ABS_A_VALUE_CHAIN_PORT=4570;
+	public static String ABS_GET_DEFAULT_METHOD="/";
+	public static String ABS_GET_ALL_METHOD="/all";
+	public static String ABS_GET_LAST_METHOD="/getlast";
+	public static String ABS_GET_BY_INDEX_METHOD="/getbyindex/";
+	public static String ABS_DEFAULT_POST_METHOD="/";
+
 }
