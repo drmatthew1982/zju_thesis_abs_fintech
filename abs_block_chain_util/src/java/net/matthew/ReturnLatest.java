0eaf382f54ce66e8250384fbc8ffcde392898496
Matthew Z <drmatthew1982@163.com>
2019-04-12 01:01:11 +0800

    Add new static argument

---
+++ abs_block_chain_util/src/java/net/matthew/ReturnLatest.java
@@ -0,0 +1,32 @@
+package net.matthew;
+
+public class ReturnLatest {
+	String code;
+	String capital;
+	String reputation;
+	String value;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public String getReputation() {
+		return reputation;
+	}
+	public void setReputation(String reputation) {
+		this.reputation = reputation;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
--- abs_block_chain_util/src/java/net/matthew/Static_Value.java
+++ abs_block_chain_util/src/java/net/matthew/Static_Value.java
@@ -9,10 +9,14 @@ public class Static_Value {
 	public static int ABS_A_REPUTATIONL_CHAIN_PORT=4569;
 	public static String ABS_A_VALUE_CHAIN_DOMAIN="localhost";
 	public static int ABS_A_VALUE_CHAIN_PORT=4570;
-	public static String ABS_GET_DEFAULT_METHOD="/";
-	public static String ABS_GET_ALL_METHOD="/all";
-	public static String ABS_GET_LAST_METHOD="/getlast";
-	public static String ABS_GET_BY_INDEX_METHOD="/getbyindex/";
-	public static String ABS_DEFAULT_POST_METHOD="/";
+	public static String ABS_GET_DEFAULT_METHOD="";
+	public static String ABS_GET_ALL_METHOD="all";
+	public static String ABS_GET_LAST_METHOD="getlast";
+	public static String ABS_GET_BY_INDEX_METHOD="getbyindex/";
+	public static String ABS_DEFAULT_POST_METHOD="";
+	public static String HTTP="http://";
+	public static String TYPE_CAPITAL = "capital";
+	public static String TYPE_REPUTATION = "repitation";
+	public static String TYPE_VALUE = "value";
 
 }
