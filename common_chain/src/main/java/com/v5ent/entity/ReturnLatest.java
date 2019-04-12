d910ef83e322c82d7ab5e9578b12e224a5827ddf
Matthew Z <drmatthew1982@163.com>
2019-04-12 14:12:35 +0800

    Add Return for get Latest

---
+++ common_chain/src/main/java/com/v5ent/entity/ReturnLatest.java
@@ -0,0 +1,33 @@
+package com.v5ent.entity;
+
+public class ReturnLatest {
+	String code;
+	Block capital;
+	Block reputation;
+	Block value;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public Block getCapital() {
+		return capital;
+	}
+	public void setCapital(Block capital) {
+		this.capital = capital;
+	}
+	public Block getReputation() {
+		return reputation;
+	}
+	public void setReputation(Block reputation) {
+		this.reputation = reputation;
+	}
+	public Block getValue() {
+		return value;
+	}
+	public void setValue(Block value) {
+		this.value = value;
+	}
+	
+}
